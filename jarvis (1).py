8fa084c83f81f478e15d1b2c07561fcc48cadcad
landenthemick <landenmick1@gmail.com>
2021-01-07 10:30:49 +0700

    jarvis

---
+++ jarvis (1).py
@@ -0,0 +1,104 @@
+import pyttsx3
+import speech_recognition as sr 
+import datetime
+import wikipedia 
+import webbrowser
+import smtplib
+import os
+import subprocess
+from email.message import EmailMessage
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'google' in query:
+            webbrowser.open("google.com")
+
+        elif 'netflix' in query:
+            webbrowser.open("netflix.com")   
+        elif 'who are you' in query:
+            speak("I am a virtual assistant developed and programmed by Sidak Arora")
+
+
+        # elif 'play music' in query:
+        #     music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+        #     songs = os.listdir(music_dir)
+        #     print(songs)    
+        #     os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+        
+        elif 'calculator' in query:
+            subprocess.Popen('C:\\Windows\\System32\\calc.exe')
+        elif 'team' in query: 
+            subprocess.Popen('C:/Users/sidak/Downloads/Teams_windows_x64.exe')
+        elif 'world' in query: 
+            subprocess.Popen('')
+
+        elif 'udemy' in query:
+            webbrowser.open("udemy.com")
+        elif 'leave' in query:
+            speak("Have a good day sir.")
+            break
+        
+
+        
+        
+
+
+        
