c01029ca725e1d2b054e58f7352c60337e35baef
Harold Karibiye <haroldkaribiye@gmail.com>
2021-01-15 08:58:28 +0000

    modified webbrowser to use chrome, grammar fixes

--- jarvis (1).py
+++ jarvis (1).py
@@ -7,29 +7,36 @@ import smtplib
 import os
 import subprocess
 from email.message import EmailMessage
+import getpass
 
+# dependencies 
+# pip install SpeechRecognition
+# pip install wikipedia
+# pip install pyttsx3
+# pip install --user pywin 
+# pywin install pyaudio 
+
+username = getpass.getuser()
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
-
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning!")
 
-    elif hour>=12 and hour<18:
+    elif hour>=12 and hour<16:
         speak("Good Afternoon!")   
 
     else:
         speak("Good Evening!")  
 
-    speak("I am Jarvis Sir. Please tell me how may I help you")       
+    speak("I am Jarvis. Please tell me, how may I help you?")       
 
 def takeCommand():
     r = sr.Recognizer()
@@ -49,26 +56,42 @@ def takeCommand():
         return "None"
     return query
 
+def chrome(chrome_path, url):
+    webbrowser.get(using = chrome_path).open(url)
+    
+def default(url):
+    webbrowser.open(url)
+
 if __name__ == "__main__":
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    end = ("quit", "close", "leave")
+    done = False
     wishMe()
-    while True:
+    while not done:
+        url = ""
         query = takeCommand().lower()
+        for val in end:
+            if val == query:
+                speak("Have a good day.")
+                done = True  
+
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
+            results = wikipedia.summary(query, sentences=3)
             speak("According to Wikipedia")
             print(results)
             speak(results)
 
         elif 'youtube' in query:
-            webbrowser.open("youtube.com")
+            url = "youtube.com"
 
         elif 'google' in query:
-            webbrowser.open("google.com")
+            url = "google.com"
 
         elif 'netflix' in query:
-            webbrowser.open("netflix.com")   
+            url = "netflix.com" 
+
         elif 'who are you' in query:
             speak("I am a virtual assistant developed and programmed by the Cornerstone team")
 
@@ -81,21 +104,36 @@ if __name__ == "__main__":
 
         elif 'time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
+            speak(f"The time is {strTime}")
         
         elif 'calculator' in query:
             subprocess.Popen('C:\\Windows\\System32\\calc.exe')
-        elif 'team' in query: 
-            subprocess.Popen('C:/Users/sidak/Downloads/Teams_windows_x64.exe')
-        elif 'world' in query: 
-            subprocess.Popen('')
+
+        elif 'open teams' in query: 
+            try:
+                subprocess.Popen(f'C:/Users/{username}/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"')
+            except:
+                try:
+                    subprocess.Popen(f'C:/Users/{username}/Downloads/Teams_windows_x64.exe')
+                except:
+                    try:
+                        subprocess.Popen(f'C:/Users/{username.replace(" ", "")}/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"')
+                    except:
+                        subprocess.Popen(f'C:/Users/{username.replace(" ", "")}/Downloads/Teams_windows_x64.exe')
+        
+        # elif 'world' in query: 
+        #     subprocess.Popen('')
 
         elif 'udemy' in query:
-            webbrowser.open("udemy.com")
-        elif 'leave' in query:
-            speak("Have a good day sir.")
-            break
+            url = "udemy.com" 
         
+        if url:
+            try:
+                chrome(chrome_path, url)
+            except:
+                default(url)
+            
+        # ideas --> spotify, Jarvis? Might need our own name yk, directly query google, jokes/easter eggs if long pause or something 
 
         
         
