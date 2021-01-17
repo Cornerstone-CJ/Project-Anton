4e55750ddb7a6cb827aa1e46aa7422ad2878325f
sidak706 <sidaksarora@gmail.com>
2021-01-16 23:53:40 -0800

    Graphing with Jarvis

--- jarvis (1).py
+++ jarvis (1).py
@@ -119,27 +119,28 @@ if __name__ == "__main__":
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"The time is {strTime}")
         
-        # elif 'plot' or 'graph' in query:
-        #     speak("I can only plot linear regression graphs. Please enter the x axis values below")
-        #     x=list(map(int,input("X Values").split(","))) 
-        #     speak("Please enter the y axis values below")
-        #     y=list(map(int,input("Y Values").split(","))) 
-        #     if len(x)!= len(y):
-        #         while len(x)!= len(y):
-        #             if len(x)> len(y):
-        #                 x.pop()
-        #             else: 
-        #                 y.pop()
-        #     plt.plot(x, y)
-        #     speak("Please enter the names of x and y labels below")
-        #     xlab= input("X label: ")
-        #     ylab= input("Y label: ")
-        #     plt.xlabel(xlab)
-        #     plt.ylabel(ylab)
-        #     speak("Enter the name of your graph")
-        #     title= input("Name: ")
-        #     plt.title(title)
-        #     plt.show()
+        elif 'graph' in query:
+            speak("I can only plot linear regression graphs. Please enter the x axis values below")
+            x=list(map(int,input("X Values: ").split(","))) 
+            speak("Please enter the y axis values below")
+            y=list(map(int,input("Y Values").split(","))) 
+            if len(x)!= len(y):
+                while len(x) != len(y):
+                    if len(x)> len(y):
+                        x.pop()
+                    else: 
+                        y.pop()
+
+            plt.plot(x, y)
+            speak("Please enter the names of x and y labels below")
+            xlab= input("X label: ")
+            ylab= input("Y label: ")
+            plt.xlabel(xlab)
+            plt.ylabel(ylab)
+            speak("Enter the name of your graph")
+            title= input("Name: ")
+            plt.title(title)
+            plt.show()
         elif 'calculator' in query:
             subprocess.Popen('C:\\Windows\\System32\\calc.exe')
 
